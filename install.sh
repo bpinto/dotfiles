b5f287893936125ee5248bdf7e827146f7d21399
Bruno Pinto <bpinto@thoughtworks.com>
2012-05-07 23:13:14 -0300

    linking oh_my_zsh custom folder

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ DOT_FILES=".gvimrc .vimrc .zshrc"
 for file in $DOT_FILES; do
   ln -s ~/Documents/dotfiles/$file ~/$file
 done
+
+rm -r ~/.oh-my-zsh/custom
+ln -s ~/Documents/dotfiles/oh_my_zsh/ ~/.oh-my-zsh/custom
