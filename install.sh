cbd6346ebcf6a02d776d430b4529ef918869c9e8
Bruno Pinto <bpinto@thoughtworks.com>
2012-05-08 21:25:19 -0300

    Files dir relative to the installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
 DOT_FILES=".gitconfig .gvimrc .vimrc .zshrc"
 for file in $DOT_FILES; do
-  ln -s ~/Documents/dotfiles/$file ~/$file
+  ln -s $DIR/$file ~/$file
 done
 
 rm -r ~/.oh-my-zsh/custom
-ln -s ~/Documents/dotfiles/oh_my_zsh/ ~/.oh-my-zsh/custom
+ln -s $DIR/oh_my_zsh/ ~/.oh-my-zsh/custom
