dd23ef56bc1a6ab8c0aec21282613fcea3fbbe89
Bruno Pinto <bpinto@thoughtworks.com>
2012-07-03 05:15:29 -0300

    add-remote-jiji function

--- oh_my_zsh/iba.zsh
+++ oh_my_zsh/iba.zsh
@@ -1,6 +1,11 @@
 alias remote="GO_ENVIRONMENT_NAME=bpinto headless=false SELENIUM_GRID=http://10.10.100.66:4444/wd/hub redis_host=vm100-102.sc01.thoughtworks.com bundle exec"
 alias morning="rake morning"
 
+add-remote-jiji() {
+  PROJECT=$(basename $PWD)
+  git remote add 'jiji' "twer@xfwang.local:~/code/iba_2_0/$PROJECT"
+}
+
 run() {
   SERVER=$(basename $PWD)
   PORT=$(cd .. &>/dev/null && exec rake -D | grep -E "Start.*$SERVER.*port: .*" | sed -E 's/.*port: ([0-9]+).*/\1/')
