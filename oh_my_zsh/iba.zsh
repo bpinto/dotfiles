1b700ed8939babcdbb28c9eaf7335ce13854af11
Bruno Pinto <bpinto@thoughtworks.com>
2012-07-17 16:46:32 -0300

    adding oracle env variables

--- oh_my_zsh/iba.zsh
+++ oh_my_zsh/iba.zsh
@@ -1,6 +1,3 @@
-alias tunnel-crp='ssh -L 6521:10.80.3.201:1521 ess61'
-alias tunnel-ftf='ssh -L 7521:10.195.1.124:1521 ess61'
-
 add-remote-jiji() {
   PROJECT=$(basename $PWD)
   git remote add 'jiji' "twer@xfwang.local:~/code/iba_2_0/$PROJECT"
@@ -20,3 +17,18 @@ run-background() {
 
 export RUBYOPT=-Ku
 export PATH=$ORACLE_HOME:$PATH
+
+export NLS_LANG="AMERICAN_AMERICA.UTF8"
+export ORACLE_HOME=/opt/oracle/instantclient_10_2
+export DYLD_LIBRARY_PATH=$ORACLE_HOME
+export LD_LIBRARY_PATH=$ORACLE_HOME
+export PATH=$ORACLE_HOME:$PATH
+
+alias sqlplus='rlwrap sqlplus'
+
+FTF_CONN="FTF/FTF@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=7521))(CONNECT_DATA=(SID=TD153)))"
+alias sqlplus-ftf="sqlplus '$FTF_CONN'"
+
+alias tunnel-crp='ssh -L 6521:10.80.3.201:1521 ess61'
+alias tunnel-ftf='ssh -L 7521:10.195.1.124:1521 ess61'
+
