a4f9a56a1f2dc9da72a71ad39b95864090660c06
Bruno Pinto <bpinto@thoughtworks.com>
2012-07-19 23:49:06 -0300

    Error report when no subtitle available to extrack

--- oh_my_zsh/plugins/mkv/mkv.plugin.zsh
+++ oh_my_zsh/plugins/mkv/mkv.plugin.zsh
@@ -3,6 +3,10 @@ function subtitle-extract {
     FILE=$x
     FILENAME=${FILE%.*}
     TRACK_NUMBER=`mkvinfo $FILE | grep -B 2 subtitles | egrep -o '.*Track number: ([0-9]+)' | sed -E 's/.*([0-9]+)/\1/'`
-    mkvextract tracks $FILE $TRACK_NUMBER:$FILENAME.srt
+    if [[ -n $TRACK_NUMBER ]]; then
+      mkvextract tracks $FILE $TRACK_NUMBER:$FILENAME.srt
+    else
+      echo 'No subtitles available.'
+    fi
   done
 }
