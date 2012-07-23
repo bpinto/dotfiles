24f16a343aff9307e904f1b41ed5104a010dda6b
Bruno Pinto <bpinto@thoughtworks.com>
2012-07-23 01:28:15 -0300

    appending robbyrussell theme to function path

--- config.fish
+++ config.fish
@@ -1,3 +1,6 @@
 set --erase fish_greeting
 set fish_function_path $fish_function_path /Users/bpinto/.oh-my-fish/functions
 set fish_function_path $fish_function_path /Users/bpinto/.oh-my-fish/functions/*
+
+# Theme
+set fish_function_path /Users/bpinto/.oh-my-fish/themes/robbyrussell $fish_function_path
