85968478e029d52d27dfa223154505ee8e4fb18e
Bruno Pinto <bpinto@thoughtworks.com>
2012-07-23 20:31:15 -0300

    defining fish plugin and theme

--- config.fish
+++ config.fish
@@ -1,7 +1,14 @@
+# Remove fish default greeting
 set --erase fish_greeting
 
 # Path to your oh-my-fish.
 set FISH $HOME/.oh-my-fish
+
+# Plugins
+set FISH_PLUGINS git
+
+# Theme
+set FISH_THEME robbyrussell
 
 # Load oh-my-fish cofiguration.
 . $FISH/oh-my-fish.fish
