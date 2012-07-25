975386f2bd1246986cc20877400a5880b4b5800a
Bruno Pinto <bpinto@thoughtworks.com>
2012-07-25 01:08:34 -0300

    Fish custom folder defined

--- config.fish
+++ config.fish
@@ -10,5 +10,8 @@ set FISH_PLUGINS autojump cd git rvm
 # Theme
 set FISH_THEME robbyrussell
 
+# Path to your custom folder.
+set FISH_CUSTOM $HOME/Documents/Bruno/dotfiles/oh-my-fish
+
 # Load oh-my-fish cofiguration.
 . $FISH/oh-my-fish.fish
