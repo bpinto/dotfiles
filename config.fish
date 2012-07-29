53f45992e8d6206819bdc55fa707fbf6c55444e9
Bruno Pinto <bpinto@thoughtworks.com>
2012-07-29 16:31:46 -0300

    lower case variable names for non-exported variables

--- config.fish
+++ config.fish
@@ -2,16 +2,16 @@
 set --erase fish_greeting
 
 # Path to your oh-my-fish.
-set FISH $HOME/.oh-my-fish
+set fish_path $HOME/.oh-my-fish
 
 # Plugins
-set FISH_PLUGINS autojump git iba osx rbenv
+set fish_plugins autojump git iba osx rbenv
 
 # Theme
-set FISH_THEME robbyrussell
+set fish_theme robbyrussell
 
 # Path to your custom folder.
-set FISH_CUSTOM $HOME/Documents/Bruno/dotfiles/oh-my-fish
+set fish_custom $HOME/Documents/Bruno/dotfiles/oh-my-fish
 
 # Load oh-my-fish cofiguration.
-. $FISH/oh-my-fish.fish
+. $fish_path/oh-my-fish.fish
