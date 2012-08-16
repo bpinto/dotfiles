62662ae15b0cbfdd67375a49a9a7c065663c92c2
Bruno Pinto <bpinto@thoughtworks.com>
2012-08-15 23:41:52 -0300

    solarized colors for fish shell

---
+++ solarized_colors.fish
@@ -0,0 +1,20 @@
+set fish_color_autosuggestion normal
+set fish_color_command green
+set fish_color_comment normal --bold
+set fish_color_cwd red --bold #not used?
+set fish_color_cwd_root red --bold #not used?
+set fish_color_error red
+set fish_color_escape red --bold #not used?
+set fish_color_history_current normal
+set fish_color_match red --bold #not used? red --bold
+set fish_color_normal normal
+set fish_color_operator normal
+set fish_color_param cyan
+set fish_color_quote yellow
+set fish_color_redirection normal
+set fish_color_search_match red --bold #not used?
+set fish_color_valid_path red --bold #not used?
+set fish_pager_color_completion cyan --bold
+set fish_pager_color_description normal
+set fish_pager_color_prefix cyan
+set fish_pager_color_progress red --bold #not used?
