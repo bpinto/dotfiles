62b1a6b44e0cc57b9ba253403a972f1bdfd0e38e
Bruno Pinto <bpinto@thoughtworks.com>
2012-08-15 23:27:29 -0300

    adding brew plugin

--- config.fish
+++ config.fish
@@ -5,7 +5,7 @@ set --erase fish_greeting
 set fish_path $HOME/.oh-my-fish
 
 # Plugins
-set fish_plugins autojump bundler iba osx rake rbenv
+set fish_plugins autojump brew bundler iba osx rake rbenv
 
 # Theme
 set fish_theme robbyrussell
