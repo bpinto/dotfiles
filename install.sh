aec94804a6147eb62d4e6dc29b1f3f61731d3822
Bruno Pinto <bpinto@thoughtworks.com>
2012-12-17 14:05:30 -0200

    configuring fish on install script

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ for file in $DOT_FILES; do
   ln -s $DIR/$file ~/$file
 done
 
+ln -s $DIR/config.fish ~/.config/fish/config.fish
+
 rm -r ~/.oh-my-zsh/custom
 ln -s $DIR/oh_my_zsh/ ~/.oh-my-zsh/custom
