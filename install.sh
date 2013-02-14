770d7367a1573ae70c0251d6bfa1a786da33343b
Bruno Pinto <bpinto@thoughtworks.com>
2013-02-14 20:46:02 -0200

    removing oh my zsh

--- install.sh
+++ install.sh
@@ -2,12 +2,9 @@
 
 DIR=$( cd "$( dirname "$0" )" && pwd )
 
-DOT_FILES=".ackrc .editrc .gitconfig .gitignore .gvimrc .vimrc .zshrc"
+DOT_FILES=".ackrc .editrc .gitconfig .gitignore .gvimrc .vimrc"
 for file in $DOT_FILES; do
   ln -s $DIR/$file ~/$file
 done
 
 ln -s $DIR/config.fish ~/.config/fish/config.fish
-
-rm -r ~/.oh-my-zsh/custom
-ln -s $DIR/oh_my_zsh/ ~/.oh-my-zsh/custom
--- oh_my_zsh/plugins/wifi/wifi.plugin.zsh
+++
@@ -1,7 +0,0 @@
-function wifi {
-  if [[ -n $(airport -I | grep "state: init" 2>/dev/null) ]]; then
-    if [[ -n $(airport -s | grep "linksys" 2>/dev/null) ]]; then
-      (exec networksetup -setairportnetwork en1 linksys 2>/dev/null)
-    fi
-  fi
-}
