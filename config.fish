822d0b82fe0fc6d50fd7b774731a4093b261f9d3
Bruno Pinto <bpinto@thoughtworks.com>
2013-03-05 17:29:01 -0300

    less color does not need to be a plugin

--- config.fish
+++ config.fish
@@ -5,7 +5,7 @@ set --erase fish_greeting
 set fish_path $HOME/.oh-my-fish
 
 # Plugins
-set fish_plugins brew bundler cliniko less osx rake rbenv z
+set fish_plugins brew bundler cliniko osx rake rbenv z
 
 # Theme
 set fish_theme robbyrussell
