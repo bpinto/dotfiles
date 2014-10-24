b342c9cf10d54d2c312c653ccb6c0218ce4333aa
Bruno Pinto <brunoferreirapinto@gmail.com>
2014-10-24 08:26:34 -0200

    adding boot2docker plugin

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ set --erase fish_greeting
 set fish_path $HOME/.oh-my-fish
 
 # Plugins
-set fish_plugins bak brew bundler cliniko dpaste extract ndenv osx rails rbenv z boxen jump
+set fish_plugins bak boot2docker brew bundler cliniko dpaste extract ndenv osx rails rbenv z boxen
 
 # Theme
 set fish_theme l
