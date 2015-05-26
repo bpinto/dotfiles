84da5ce1bdc5ec72bc1cf7158c4365b6940d1b69
Bruno Pinto <brunoferreirapinto@gmail.com>
2015-05-26 19:31:53 +0100

    new oh-my-fish plugin and themes config

--- fish/config.fish
+++ fish/config.fish
@@ -4,14 +4,23 @@ set --erase fish_greeting
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
-# Plugins
-set fish_plugins brew boxen boot2docker bundler cliniko dpaste extract ndenv rails random theme z rbenv
-
-# Theme
-set fish_theme l
-
 # Path to your custom folder.
 set fish_custom $HOME/Dropbox/Documents/Bruno/dotfiles/oh-my-fish
 
-# Load oh-my-fish cofiguration.
+# Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+Theme 'l'
+Plugin 'brew'
+Plugin 'boxen'
+Plugin 'boot2docker'
+Plugin 'bundler'
+Plugin 'cliniko'
+Plugin 'dpaste'
+Plugin 'extract'
+Plugin 'ndenv'
+Plugin 'rails'
+Plugin 'random'
+Plugin 'theme'
+Plugin 'z'
+Plugin 'rbenv'
