fec79c141da9473c49f5e68f937ee0f7b64a6e4d
Bruno Pinto <brunoferreirapinto@gmail.com>
2016-01-26 15:21:06 +0000

    Remove old commented out code

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,5 @@
 # Path to Oh My Fish install.
 set -gx OMF_PATH /Users/bpinto/.local/share/omf
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /Users/bpinto/.config/omf
-
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
