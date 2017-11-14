8c8610d29a9657e1dd8d3d0dbd96b172781eb0eb
Bruno Pinto <brunoferreirapinto@gmail.com>
2017-11-14 22:50:21 +0000

    Enable vi-mode (hybrid) for fish shell

---
+++ .config/omf/pkg/gentoo/functions/fish_default_mode_prompt.fish
@@ -0,0 +1,23 @@
+function fish_default_mode_prompt --description 'Display the default mode for the prompt'
+	# Do nothing if not in vi mode
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+        switch $fish_bind_mode
+            case default
+                set_color --bold red
+                echo 'N'
+            case insert
+                set_color --bold green
+                echo 'I'
+            case replace_one
+                set_color --bold green
+                echo 'R'
+            case visual
+                set_color --bold brmagenta
+                echo 'V'
+        end
+        set_color normal
+        echo -n ' '
+    end
+end
+
