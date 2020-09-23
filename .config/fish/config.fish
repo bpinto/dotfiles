cd63f9a577967c819d2c21ff86a5cdd99501319f
Bruno Pinto <bruno@bpinto.com>
2020-09-23 01:16:35 +0100

    XQuartz

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,15 @@ set fish_greeting
 if not set -q fish_user_paths
   # Add brew binaries to PATH
   set -U fish_user_paths /usr/local/bin /usr/local/sbin
+
+  # Add XQuartz binaries to PATH
+  set -U fish_user_paths $fish_user_paths /usr/X11/bin
+end
+
+if status --is-interactive
+  # Do not ignore hidden files when filtering files (e.g. vim integration)
+  set -gx FZF_DEFAULT_COMMAND "rg --files --hidden -g'!.git'"
+
+  # XQuartz
+  set -gx DISPLAY ":0"
 end
