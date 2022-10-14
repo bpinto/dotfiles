e0fe7adda4b877e97e2e142b05d46afbe39502c3
Bruno Pinto <bruno@bpinto.com>
2022-10-14 15:44:11 +0100

    refactor: client.resolved_capabilities has been deprecated

--- .config/nvim/lua/my_modules/lsp.lua
+++ .config/nvim/lua/my_modules/lsp.lua
@@ -14,6 +14,8 @@ lsp.handlers['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_hel
 
 vim.opt.updatetime = 300 -- set inactivity time to 300ms
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 local on_attach = function(client, bufnr)
   local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
@@ -25,26 +27,38 @@ local on_attach = function(client, bufnr)
   map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 
-  if client.resolved_capabilities.document_formatting then
-    map('n', '<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-  elseif client.resolved_capabilities.document_range_formatting then
-    map('n', '<leader>=', '<cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_buf_create_user_command(bufnr, "LspFormatting", function()
+      lsp.buf.format({ bufnr = bufnr })
+    end, {})
+
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      command = "LspFormatting",
+    })
   end
 
   -- Show line diagnostics after inactivity
   vim.cmd('autocmd CursorHold <buffer> lua vim.diagnostic.open_float()')
 
-  -- Show singature help after inactivity on insertion mode
+  -- Show signature help after inactivity on insertion mode
   vim.cmd('autocmd CursorHoldI <buffer> silent! lua vim.lsp.buf.signature_help()')
 end
 
 null_ls.setup({
   on_attach = on_attach,
   sources = {
+    -- code actions
+    null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.code_actions.gitsigns,
+
+    -- diagnostics
     null_ls.builtins.diagnostics.codespell,
     null_ls.builtins.diagnostics.eslint_d,
+
+    -- formatting
     null_ls.builtins.formatting.fish_indent,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.eslint_d,
