2fba3e3f5d8fb1b51d835950e2ddc61d642c21c6
Bruno Pinto <bruno@bpinto.com>
2023-05-22 15:23:02 +0100

    feat(dockerize): support multiple services
    ```
    node:webpack
    npm:webpack
    *:app
    ```

--- .config/fish/functions/dockerize_commands.fish
+++ .config/fish/functions/dockerize_commands.fish
@@ -7,7 +7,7 @@ function dockerize_commands
     set path (_git_dir_folder)
 
     if test -n "$path"; and test -f "$path/docker-compose"
-        set app_name (cat "$path/docker-compose")
+        set app_name (cat "$path/docker-compose" | grep -E "^($argv[1]|\*)" | head -1 | cut -d':' -f2)
         command docker compose run --rm $app_name $argv
     else
         command rake $argv
