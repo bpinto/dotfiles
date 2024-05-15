3b7e8dbf81d7411c18f0fb2fd502da232b7d4e89
Bruno Pinto <bruno@bpinto.com>
2024-05-15 16:47:05 +0100

    chore: remove no longer needed color override
    Ref: https://github.com/folke/tokyonight.nvim/commit/aabb52985c316368c482deb10e1529852f7feddd

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -6,10 +6,6 @@ return {
 		opts = function()
 			return {
 				style = "storm",
-
-				on_highlights = function(hl, c)
-					hl.CursorLineNr = { fg = c.orange, bold = true }
-				end,
 			}
 		end,
 	},
