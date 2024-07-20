29648cab65ae6d0fb8f660d0242871463dbe110e
Bruno Pinto <bruno@bpinto.com>
2024-07-20 19:12:26 +0100

    feat(neovim): add markdown plugin

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		ft = { "gitcommit", "markdown" },
+		main = "render-markdown",
+		opts = {
+			file_types = { "gitcommit", "markdown" },
+		},
+	},
+}
