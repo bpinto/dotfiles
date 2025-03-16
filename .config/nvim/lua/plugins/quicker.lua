a1f713a367d5a94b32f198cb64d617bc317d646d
Bruno Pinto <bruno@bpinto.com>
2025-03-16 17:01:27 +0000

    feat(neovim): improve quickfix with quicker.nvim

---
+++ .config/nvim/lua/plugins/quicker.lua
@@ -0,0 +1,21 @@
+return {
+	"stevearc/quicker.nvim",
+	event = "FileType qf",
+	keys = {
+		{
+			">",
+			function()
+				require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+			end,
+			desc = "Expand quickfix context",
+		},
+		{
+			"<",
+			function()
+				require("quicker").collapse()
+			end,
+			desc = "Collapse quickfix context",
+		},
+	},
+	opts = {},
+}
