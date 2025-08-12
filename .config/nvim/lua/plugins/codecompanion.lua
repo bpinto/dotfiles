38b82c0aa377966583a8a773ac2d2b1a2025e853
Bruno Pinto <bruno@bpinto.com>
2025-08-12 22:16:50 +0100

    feat(nvim): update key maps for codecompanion

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -91,6 +91,18 @@ return {
 						},
 					},
 				},
+				inline = {
+					keymaps = {
+						accept_change = {
+							modes = { n = "ga" },
+							description = "Accept the suggested change",
+						},
+						reject_change = {
+							modes = { n = "gr" },
+							description = "Reject the suggested change",
+						},
+					},
+				},
 			},
 
 			opts = {
